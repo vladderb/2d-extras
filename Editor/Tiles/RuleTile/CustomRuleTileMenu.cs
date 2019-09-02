d4d5c3720142af52b3a753cd3ba19258672a87e1
ChuanXin-Unity <chuanxin@unity3d.com>
2019-09-02 14:28:55 +0800

    -Revert to pre-2019 version of CreateCustomRuleTile

--- Editor/Tiles/RuleTile/CustomRuleTileMenu.cs
+++ Editor/Tiles/RuleTile/CustomRuleTileMenu.cs
@@ -4,8 +4,15 @@
     {
         [MenuItem("Assets/Create/Custom Rule Tile Script", false, 89)]
         static void CreateCustomRuleTile()
+        { 
+            CreateScriptAsset("Packages/com.unity.2d.tilemap.extras/Editor/Tiles/RuleTile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
+        }
+        
+        static void CreateScriptAsset(string templatePath, string destName)
         {
-            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/Tilemap/Tiles/Rule Tile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
+            typeof(ProjectWindowUtil)
+                .GetMethod("CreateScriptAsset", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)
+                .Invoke(null, new object[] { templatePath, destName });
         }
     }
 }
